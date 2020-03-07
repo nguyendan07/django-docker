40d2df853dff7108c6aacb84e15156149d935c5b
nguyendan07 <nguyendan@hotmail.com.vn>
2020-03-07 17:51:13 +0700

    create a .env.dev file

--- djdocker/settings.py
+++ djdocker/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm$q1g=-#j2leqmkle1^)6gpvg-yez!fp+^n5mondl+)ub5m1r8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
